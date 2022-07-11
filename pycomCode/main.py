7f89acb8a5beacad7bfb13d12e457933acb70d3b
Vasco de Graaff <vascodegraaff@gmail.com>
2022-07-11 10:13:29 +0000

    code with sensor and node

--- pycomCode/main.py
+++ pycomCode/main.py
@@ -1,31 +1,107 @@
-#!/usr/bin/env python
-#
-# Copyright (c) 2019, Pycom Limited.
-#
-# This software is licensed under the GNU GPL version 3 or any
-# later version, with permitted additional terms. For more information
-# see the Pycom Licence v1.0 document supplied with this file, or
-# available at https://www.pycom.io/opensource/licensing
-#
+# Lora Node
+from network import LoRa
+import socket
+import binascii
+import struct
+import time
+import pycom
 
-""" LoPy LoRaWAN Nano Gateway example usage """
+# SPS30 Sensor
+from time import time, sleep
+import sys
+from SPS30 import SPS30
 
-import config
-from nanogateway import NanoGateway
+# import config
 
-if __name__ == '__main__':
-    nanogw = NanoGateway(
-        id=config.GATEWAY_ID,
-        frequency=config.LORA_FREQUENCY,
-        datarate=config.LORA_GW_DR,
-        ssid=config.WIFI_SSID,
-        password=config.WIFI_PASS,
-        server=config.SERVER,
-        port=config.PORT,
-        ntp_server=config.NTP,
-        ntp_period=config.NTP_PERIOD_S
-        )
+LORA_FREQUENCY = 433175000
+LORA_NODE_DR = 5
 
-    nanogw.start()
-    nanogw._log('You may now press ENTER to enter the REPL')
-    input()
+lora = LoRa(mode=LoRa.LORAWAN, region=LoRa.EU433)  # Australia
+
+# create an ABP authentication params
+dev_addr = struct.unpack(">l", binascii.unhexlify("260B0701"))[0]
+nwk_swkey = binascii.unhexlify("BF6F735B7728AD9AEA7DAE6B423E7E15")
+app_swkey = binascii.unhexlify("EF1D59AC28332E8C836302F94C89407C")
+
+# remove all the non-default channels
+for i in range(3, 16):
+    lora.remove_channel(i)
+
+# set the 3 default channels to the same frequency
+lora.add_channel(0, frequency=LORA_FREQUENCY, dr_min=5, dr_max=5)
+lora.add_channel(1, frequency=LORA_FREQUENCY, dr_min=5, dr_max=5)
+lora.add_channel(2, frequency=LORA_FREQUENCY, dr_min=5, dr_max=5)
+
+# join a network using ABP (Activation By Personalization)
+lora.join(activation=LoRa.ABP, auth=(dev_addr, nwk_swkey, app_swkey))
+
+# create a LoRa socket
+s = socket.socket(socket.AF_LORA, socket.SOCK_RAW)
+
+# set the LoRaWAN data rate
+s.setsockopt(socket.SOL_LORA, socket.SO_DR, 5)
+
+# make the socket blocking
+s.setblocking(False)
+pycom.heartbeat(False)
+pycom.rgbled(0xFF00)
+
+print("Started LoRa Node at :", LORA_FREQUENCY, "Hz")
+
+
+interval = 60
+sample = 60
+debug = True
+try:
+    from machine import UART, Pin
+
+    pins = ("P4", "P3", "P19")
+    print("Using pins: %s" % str(pins))
+    Pin(pins[2], mode=Pin.OUT).value(1)
+    sleep(1)
+    port = UART(1, baudrate=115200, pins=pins[:2], timeout_chars=20)
+except:
+    import sys
+    port = sys.argv[1]
+
+sps30 = SPS30(port=port, debug=debug, sample=sample, interval=interval)
+#print(sps30.device_info(debug=debug))
+# print(sps30.isStarted(debug=debug))
+
+class SensorData:
+    
+    def __init__(self,pm1,pm25,pm4,pm10,pm05_cnt,pm1_ccnt,pm25_cnt,pm4_cnt,pm10_cnt):
+        self.pm1 = pm1
+        self.pm25 = pm25
+        self.pm4 = pm4
+        self.pm10 = pm10
+        self.pm05_cnt = pm05_cnt
+        self.pm1_ccnt = pm1_ccnt
+        self.pm25_cnt = pm25_cnt
+        self.pm4_cnt = pm4_cnt
+        self.pm10_cnt = pm10_cnt
+    
+    def export_data(self):
+        return [self.pm25, self.pm10,self.pm25_cnt,self.pm10_cnt]
+
+for i in range(4):
+    lastTime = time()
+    print(sps30.getData(debug=debug))
+    sps30.read_measurement(debug=debug)
+    
+    now = interval + time() - lastTime
+    if now > 0:
+        sleep(now)
+
+
+
+# for i in range(200):
+
+#     pkt = b"PKT #" + bytes([i])
+#     print("Sending:", pkt)
+#     s.send(pkt)
+#     time.sleep(4)
+#     rx, port = s.recvfrom(256)
+#     if rx:
+#         print("Received: {}, on port: {}".format(rx, port))
+#     time.sleep(5)
